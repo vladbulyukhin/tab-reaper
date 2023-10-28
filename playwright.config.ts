4129aaf659b577bd977fe9bf527330b7d2f3e4a0
Vladislav Bulyukhin <vladislavb@kontent.ai>
2023-10-28 19:29:25 +0200

    e2e test improvements

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -23,10 +17,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
@@ -37,11 +27,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
--- tests/e2e/helpers/fixtures.ts
+++ tests/e2e/helpers/fixtures.ts
@@ -1,15 +1,13 @@
-import { test as base, chromium, type BrowserContext, Worker } from '@playwright/test';
 import path from 'path';
+import { test as base, chromium, type BrowserContext, Worker } from '@playwright/test';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// TODO: clear and improve
 export const test = base.extend<{
   context: BrowserContext;
   backgroundPage: Worker;
-  helperBackgroundPage: Worker;
   helperExtensionId: string;
   extensionId: string;
 }>({
@@ -29,8 +27,8 @@ export const test = base.extend<{
   },
   backgroundPage: async ({ context }: { context: BrowserContext }, use: (bg: Worker) => Promise<void>) => {
     const serviceWorkers = context.serviceWorkers();
-
     let background = serviceWorkers.find((w) => w.url().includes('service-worker-loader.js'));
+
     if (!background) {
       const serviceWorker = await context.waitForEvent('serviceworker');
       background = serviceWorker.url().includes('service-worker-loader.js') ? serviceWorker : await context.waitForEvent('serviceworker');
@@ -42,7 +40,7 @@ export const test = base.extend<{
     const extensionId = backgroundPage.url().split('/')[2];
     await use(extensionId);
   },
-  helperBackgroundPage: async ({ context }: { context: BrowserContext }, use: (bg: Worker) => Promise<void>) => {
+  helperExtensionId: async ({ context }: { context: BrowserContext }, use) => {
     const serviceWorkers = context.serviceWorkers();
 
     let background = serviceWorkers.find((w) => w.url().includes('helper'));
@@ -51,11 +49,8 @@ export const test = base.extend<{
       background = serviceWorker.url().includes('helper') ? serviceWorker : await context.waitForEvent('serviceworker');
     }
 
-    await use(background);
-  },
-  helperExtensionId: async ({ helperBackgroundPage }, use) => {
-    const extensionId = helperBackgroundPage.url().split('/')[2];
-    await use(extensionId);
+    const helperExtensionId = background.url().split('/')[2];
+    await use(helperExtensionId);
   },
 });
 
