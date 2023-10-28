ee23929b757670f34bbd1bc97bf2babc813b4f66
Vladislav Bulyukhin <vladislavb@kontent.ai>
2023-10-28 19:29:25 +0200

    e2e test improvements

---
+++ tests/e2e/helpers/extension/helper.js
@@ -0,0 +1,5 @@
+/**
+ * Service Worker Placeholder for E2E Test Helper Extension.
+ * This empty file is crucial for identifying the helper extension during e2e tests.
+ * DO NOT DELETE OR MODIFY.
+ */
